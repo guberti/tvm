cdda1a4c51b70f3d54ca82be5bc6cf2507bb782d
Gavin Uberti <gavin.uberti@gmail.com>
2022-11-04 01:31:42 -0700

    Tensordot offset support

--- python/tvm/topi/arm_cpu/mprofile/dsp/micro_kernel/tensordot.py
+++ python/tvm/topi/arm_cpu/mprofile/dsp/micro_kernel/tensordot.py
@@ -28,14 +28,15 @@ import numpy as np
 from tvm import te, tir
 
 
-def get_c_function_name(tensor_w, kernel_dims, split_size, x_strides, options):
+def get_c_function_name(split_size, dimensions, offsets, x_strides, options):
     """Gets the C function name of the tensordot function."""
+    tensor_w, kernel_h, kernel_w = dimensions
     return (
         f"tensordot_opt_x{split_size}_int16_w{tensor_w}_"
-        + f"{kernel_dims[0]}x{kernel_dims[1]}"
+        + f"{kernel_dims[0]}x{kernel_dims[1]}_"
+        + "".join(map(str, offsets)) + "_"
         + (f"_{x_strides[0]}_{x_strides[1]}" if split_size > 1 else "")
         + ("_dsp" if options[0] else "")
-        + ("_2xkernel" if options[1] else "")
     )
 
 def _is_pow_2(number):
@@ -64,24 +65,27 @@ def _init_biased_accumulators(split_size):
     return f"int {joined_var_names} = *bias;"
 
 
-def _get_tensor_halfwords(tensor_w, kernel_dims, split_size, in_stride) -> Iterator:
-    kernel_h, kernel_w = kernel_dims
+def _get_tensor_halfwords(dimensions, offset, split_size, in_stride) -> Iterator:
+    tensor_w, kernel_h, kernel_w = dimensions
+
     split_max = (split_size - 1) * in_stride
     for y in range(kernel_h):
-        if y * tensor_w % 2 == 1:
+        if y * tensor_w % 2 + offset == 1:
             yield None
         for x in range(kernel_w + split_max):
             yield (y, x)
-        if (y * tensor_w + kernel_w + split_max) % 2 == 1:
+        if (y * tensor_w + kernel_w + split_max + offset) % 2 == 1:
             yield None
 
 
-def _get_kernel_halfwords(kernel_dims, _has_multi_kernel) -> Iterator:
-    kernel_h, kernel_w = kernel_dims
+def _get_kernel_halfwords(dimensions, offset) -> Iterator:
+    _tensor_w, kernel_h, kernel_w = dimensions
+    if offset == 1:
+        yield None
     for y in range(kernel_h):
         for x in range(kernel_w):
             yield (y, x)
-    if kernel_h * kernel_w % 2 == 1:
+    if (kernel_h * kernel_w + offset) % 2 == 1:
         yield None
 
 
@@ -93,6 +97,7 @@ def _get_int16_alias(position) -> str:
 
 
 def _load_tensor_vars(halfwords, tensor_w) -> Iterator[str]:
+    assert len(halfwords) % 2 == 0
     for i in range(0, len(halfwords), 2):
         var_name = "__".join(map(_get_int16_alias, halfwords[i : i + 2]))
         y, x = halfwords[i] or halfwords[i + 1]
@@ -101,6 +106,7 @@ def _load_tensor_vars(halfwords, tensor_w) -> Iterator[str]:
 
 
 def _load_kernel_vars(halfwords) -> Iterator[str]:
+    assert len(halfwords) % 2 == 0
     for i in range(0, len(halfwords), 2):
         var_name = "__".join(map(_get_int16_alias, halfwords[i : i + 2]))
         yield f"int kernel__{var_name} = kernel[{i // 2}];"
@@ -222,50 +228,61 @@ def _requantize_sums(num_sums) -> Iterator[str]:
         yield f"requant_{i} = (sum_{i} > 127) ? sum_{i} : 127;"
 
 
-def _write_out_sums(num_sums) -> Iterator[str]:
+def _write_out_sums(num_sums, offset, stride) -> Iterator[str]:
     """Writes the requantized sums to memory. Note - halfword packing here *does* help. It seems
     like it wouldn't, as doing two pipelined int16 stores takes two cycles - the same as halfword
-    packing plus a pipelined int32 store.
+    packing plus a pipelined int32 store. We still do the int16 stores when there is an output
+    stride, though.
 
     However, this lets the compiler re-order instructions to better preserve memory, as it doesn't
     like breaking apart the store instructions (as this messes up pipelining)."""
 
-    for i in range(num_sums // 2):
-        yield f"int packed_res_{i} = __builtin_arm_pkhtb(requant_{2 * i + 1}, requant_{2 * i});"
+    if stride > 1:
+        for i in range(num_sums):
+            yield f"((*short) output)[{i * stride + offset}] = (short) requant_{i};"
 
-    for i in range(num_sums // 2):
-        yield f"output[i] = packed_res_{i};"
+    else:
+        num_halfwords = (num_sums - offset) // 2
+        for i in range(num_halfwords):
+            index = 2 * i + offset
+            yield f"int packed_res_{i} = __builtin_arm_pkhtb(requant_{index + 1}, requant_{index});"
 
-    if num_sums % 2 == 1:
-        yield f"((*short) output)[{num_sums * 2}] = (short) requant_{num_sums * 2};"
+        if offset == 1:
+            yield f"((*short) output)[1] = (short) requant_0;"
 
+        for i in range(num_halfwords):
+            yield f"output[{offset + i}] = packed_res_{i};"
+
+        if (offset + num_sums) % 2 == 1:
+            yield f"((*short) output)[{num_sums * 2}] = (short) requant_{num_sums * 2};"
 
 
 def tensordot_int16_impl(
-    tensor_w: int,
-    kernel_dims: Tuple[int, int],
     split_size: int,
+    dimensions: Tuple[int, int, int],
+    offsets: Tuple[int, int, int],
     x_strides: Tuple[int, int],
-
-    options: Tuple[bool, bool],
-
+    options: Tuple[bool],
 ) -> str:
     """Code for a specialized version of tensordot, which computes `split_size` tensordot operations
     at the same time. Only works with `int16`. The generated function takes as input pointers to the
     output, tensor, and kernel, which must be word-aligned. However, the stride can be half a word.
     """
-    function_name = get_c_function_name(tensor_w, kernel_dims, split_size, x_strides, options)
+    function_name = get_c_function_name(split_size, dimensions, offsets, x_strides, options)
+    tensor_w, kernel_h, kernel_w = dimensions
+    tensor_offset, kernel_offset, output_offset = offsets
+    assert tensor_offset < 2 and kernel_offset < 2 and output_offset < 2
     in_stride, out_stride = x_strides
-    has_dsp, has_multi_kernel = options
+    has_dsp, = options
 
-    tensor_halfwords = list(_get_tensor_halfwords(tensor_w, kernel_dims, split_size, in_stride))
-    kernel_halfwords = list(_get_kernel_halfwords(kernel_dims, has_multi_kernel))
+    tensor_halfwords = list(_get_tensor_halfwords(dimensions, tensor_offset, split_size, in_stride))
+    kernel_halfwords = list(_get_kernel_halfwords(dimensions, kernel_offset))
     load_tensor_lines = _load_tensor_vars(tensor_halfwords, tensor_w)
     load_kernel_lines = _load_kernel_vars(kernel_halfwords)
 
     def gen_single_loop_macs(index):
         draft_macs_iter = _get_draft_macs(
-            kernel_dims, tensor_halfwords, kernel_halfwords, index * in_stride
+            (kernel_h, kernel_w), tensor_halfwords, kernel_halfwords, index * in_stride
         )
         if has_dsp:
             draft_macs_iter = _apply_simd_optimizations(draft_macs_iter)
@@ -273,7 +290,8 @@ def tensordot_int16_impl(
         return _expand_instruction_tuples(draft_macs_iter, index)
 
     multiply_acc_lines = chain.from_iterable(gen_single_loop_macs(i) for i in range(split_size))
-    write_out_lines = _write_sums_to_memory(split_size, out_stride)
+    requantize_lines = _requantize_sums(num_sums)
+    write_out_lines = _write_sums_to_memory(split_size, out_offset, out_stride)
 
     def insert_lines(lines):
         return ("\n" + " " * 10).join(lines)
@@ -292,6 +310,8 @@ def tensordot_int16_impl(
           {insert_lines(load_kernel_lines)}
 
           {insert_lines(multiply_acc_lines)}
+
+          {insert_lines(requantize_lines)}
 
           {insert_lines(write_out_lines)}
           return 0;
